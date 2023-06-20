d240f9a7f1ae692a825bf2e5a61061cd99bc8f64
Artem Balan <a.s.balan96@gmail.com>
2023-06-20 21:14:56 +0300

    Add simple data classes

---
+++ src/main/java/com/example/tacocloud/tacos/TacoOrder.java
@@ -0,0 +1,21 @@
+package com.example.tacocloud.tacos;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TacoOrder {
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCW;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addTaco(Taco taco) {
+        this.tacos.add(taco);
+    }
+}
