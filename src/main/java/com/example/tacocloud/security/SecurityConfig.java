0ae98ec4c63f6d0711c676d2526d08431101a3dd
Artem Balan <a.s.balan96@gmail.com>
2023-12-10 20:54:19 +0200

    Configure user authorization

--- src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.example.tacocloud.security;
 
+import com.example.tacocloud.tacos.User;
+import com.example.tacocloud.tacos.data.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -11,5 +15,14 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepo) {
+        return username -> {
+            User user = userRepo.findByUsername(username);
+            if (user != null) return user;
+            throw new UsernameNotFoundException("User ‘" + username + "’ not found");
+        };
     }
 }
