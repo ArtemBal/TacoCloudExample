0ea07bc687de44bc57e9841c41b841d3d76382ea
Artem Balan <a.s.balan96@gmail.com>
2023-12-13 19:01:58 +0200

    Configure Security

--- src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.example.tacocloud.tacos.User;
 import com.example.tacocloud.tacos.data.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -24,5 +26,19 @@ public class SecurityConfig {
             if (user != null) return user;
             throw new UsernameNotFoundException("User ‘" + username + "’ not found");
         };
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                //.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/design", "/orders").hasRole("USER")
+                .requestMatchers("/", "/**").permitAll()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                ).build();
     }
 }
